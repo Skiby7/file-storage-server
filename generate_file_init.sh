f8f1e4267cb519f57c4f7edb8c6f37f165cf8608
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-07-27 17:21:45 +0200

    added test_2 file generation

--- generate_file_init.sh
+++ generate_file_init.sh
@@ -9,26 +9,37 @@ mkdir ${CWD}/test/small_files &> /dev/null
 for i in {0..99}
 do
 	head -c 1024 < /dev/urandom > ${CWD}/test/small_files/small_${i}.txt
-	echo -e "${LINE_UP}Generati ${i+1} files"
+	NUM=$((i+1))
+	echo -e "${LINE_UP}Generati ${NUM} file piccoli"
 done
 chmod 777 -R ${CWD}/test/small_files/
 echo ""
 
 
-mkdir ${CWD}/test/medium_files
+mkdir ${CWD}/test/medium_files &> /dev/null
 for i in {0..9}
 do
 	head -c 1000000 < /dev/urandom > ${CWD}/test/medium_files/medium_${i}.txt
-	echo -e "${LINE_UP}Generati ${i+1} files"
+	NUM=$((i+1))
+	echo -e "${LINE_UP}Generati ${NUM} file medi"
 done
 chmod 777 -R ${CWD}/test/medium_files
 echo ""
 
-mkdir ${CWD}/test/large_files
+mkdir ${CWD}/test/large_files &> /dev/null
 for i in {0..4}
 do
 	head -c 50000000 < /dev/urandom > ${CWD}/test/large_files/large_${i}.txt
-	echo -e "${LINE_UP}Generati ${i+1} files"
+	NUM=$((i+1))
+	echo -e "${LINE_UP}Generati ${NUM} file grandi"
 done
-chmod 777 -R ${CWD}/test/large_files/
+mkdir ${CWD}/test/test_2 &> /dev/null
+
+head -c 400000 < /dev/urandom > ${CWD}/test/test_2/eviction_file_0.txt
+head -c 400000 < /dev/urandom > ${CWD}/test/test_2/eviction_file_1.txt
+head -c 400000 < /dev/urandom > ${CWD}/test/test_2/eviction_file_2.txt
+head -c 500000 < /dev/urandom > ${CWD}/test/test_2/initial_file_0.txt
+echo -e "${LINE_UP}Generati i file per il test 2"
+
+chmod 777 -R ${CWD}/test/test_2/
 echo -e "Fatto!\n"
