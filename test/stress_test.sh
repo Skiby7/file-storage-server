16b056994ffa1fb9f99a66c911a6f8b42e67f29a
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-07-27 17:05:36 +0200

    python not supported in VM, using bash

--- test/stress_test.sh
+++ test/stress_test.sh
@@ -4,13 +4,13 @@ while true
 do
 	SMALL_FILE_NUM=$((RANDOM % 100))
 	MEDIUM_FILE_NUM=$((RANDOM % 10))
-	bin/client -f /tmp/socket.sk -w ${CWD}/small_files/small_${SMALL_FILE_NUM}.dat -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.dat -R 5 -d ${CWD}/output_stress_test -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.dat -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.dat -W ${CWD}/test_2/initial_file_0.dat -u ${CWD}/test_2/initial_file_0.dat -l ${CWD}/test_2/initial_file_0.dat -c ${CWD}/test_2/initial_file_0.dat -t 500 &> /dev/null
-	# bin/client -f /tmp/socket.sk -l ${CWD}/test_2/initial_file_0.dat -c ${CWD}/test_2/initial_file_0.dat &> /dev/null
+	bin/client -f /tmp/socket.sk -w ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -R 5 -d ${CWD}/output_stress_test -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -W ${CWD}/test_2/initial_file_0.txt -u ${CWD}/test_2/initial_file_0.txt -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt -t 500 &> /dev/null
+	# bin/client -f /tmp/socket.sk -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt &> /dev/null
 	if [[ $(($RANDOM % 2)) -eq 0 ]]
 	then
-    	bin/client -f /tmp/socket.sk -l ${CWD}/small_files/small_${SMALL_FILE_NUM}.dat -c ${CWD}/small_files/small_${SMALL_FILE_NUM}.dat &> /dev/null
+    	bin/client -f /tmp/socket.sk -l ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -c ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt &> /dev/null
 	else
-    	bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.dat -c ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.dat &> /dev/null
+    	bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -c ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt &> /dev/null
 
 	fi
 
--- test/test2.sh
+++ test/test2.sh
@@ -11,12 +11,12 @@ bash -c "sleep 5 && kill -1 ${SERVER}" &
 STOP_SERVER=$!
 sleep 1
 # Write some files
-bin/client -f /tmp/socket.sk -W ${CWD}/test_2/initial_file_0.dat -u ${CWD}/test_2/initial_file_0.dat  -p  
+bin/client -f /tmp/socket.sk -W ${CWD}/test_2/initial_file_0.txt -u ${CWD}/test_2/initial_file_0.txt  -p  
 
-# bin/client -f /tmp/socket.sk -r /home/leonardo/Documents/SO/Project/file-storage-server/test/test_2/initial_file_0.dat -p 
+# bin/client -f /tmp/socket.sk -r /home/leonardo/Documents/SO/Project/file-storage-server/test/test_2/initial_file_0.txt -p 
 sleep 2
-# bin/client -f /tmp/socket.sk -W ${CWD}/test_2/eviction_files_0.dat,${CWD}/test_2/eviction_files_1.dat -D ${CWD}/test_output -u ${CWD}/test_2/evictions_file_0.dat,${CWD}/test_2/evictions_file_1.dat   -p  
-bin/client -f /tmp/socket.sk -W ${CWD}/test_2/eviction_file_0.dat,${CWD}/test_2/eviction_file_1.dat,${CWD}/test_2/eviction_file_2.dat -D ${CWD}/test_output -u ${CWD}/test_2/eviction_file_0.dat  -p  
+# bin/client -f /tmp/socket.sk -W ${CWD}/test_2/eviction_files_0.txt,${CWD}/test_2/eviction_files_1.txt -D ${CWD}/test_output -u ${CWD}/test_2/evictions_file_0.txt,${CWD}/test_2/evictions_file_1.txt   -p  
+bin/client -f /tmp/socket.sk -W ${CWD}/test_2/eviction_file_0.txt,${CWD}/test_2/eviction_file_1.txt,${CWD}/test_2/eviction_file_2.txt -D ${CWD}/test_output -u ${CWD}/test_2/eviction_file_0.txt  -p  
 
 
 
