43a7c3c6cab3a7af87670acbe733b66757f6d994
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-07-27 17:11:44 +0200

    added zconf

--- generate_file_init.sh
+++ generate_file_init.sh
@@ -6,29 +6,29 @@ CWD=$(realpath $(dirname $0))
 echo -e "\n"
 mkdir ${CWD}/test &> /dev/null
 mkdir ${CWD}/test/small_files &> /dev/null
-for i in {0..100}
+for i in {0..99}
 do
 	head -c 1024 < /dev/urandom > ${CWD}/test/small_files/small_${i}.txt
-	echo -e "${LINE_UP}Generati ${i} files"
+	echo -e "${LINE_UP}Generati ${i+1} files"
 done
 chmod 777 -R ${CWD}/test/small_files/
 echo ""
 
 
 mkdir ${CWD}/test/medium_files
-for i in {0..10}
+for i in {0..9}
 do
 	head -c 1000000 < /dev/urandom > ${CWD}/test/medium_files/medium_${i}.txt
-	echo -e "${LINE_UP}Generati ${i} files"
+	echo -e "${LINE_UP}Generati ${i+1} files"
 done
 chmod 777 -R ${CWD}/test/medium_files
 echo ""
 
 mkdir ${CWD}/test/large_files
-for i in {0..5}
+for i in {0..4}
 do
 	head -c 50000000 < /dev/urandom > ${CWD}/test/large_files/large_${i}.txt
-	echo -e "${LINE_UP}Generati ${i} files"
+	echo -e "${LINE_UP}Generati ${i+1} files"
 done
 chmod 777 -R ${CWD}/test/large_files/
 echo -e "Fatto!\n"
