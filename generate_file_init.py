3ce199172f5838c71062ac876cde291441e526f3
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-07-27 16:21:41 +0200

    added test files generator

---
+++ generate_file_init.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import random
+import string
+
+def string_generator(size, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits + ' '):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+
+path = input("Where do you want to save the files? ")
+
+try:
+	os.chdir(path)
+except:
+	os.mkdir(path)
+	os.chdir(path)
+
+try:
+	os.chdir('small_files')
+except:
+	os.mkdir('small_files')
+	os.chdir('small_files')
+
+print()
+for i in range(100):
+	f = open("small_" + str(i) + '.dat', "w+")
+	f.write(string_generator(1024))
+	f.close()
+	print("\033[AGenerated " + str(i+1) + " small files")
+os.chdir('..')
+print()
+try:
+	os.chdir('medium_files')
+except:
+	os.mkdir('medium_files')
+	os.chdir('medium_files')
+print()
+for i in range(10):
+	f = open("medium_" + str(i) + '.dat', "w+")
+	f.write(string_generator(10000000))
+	f.close()
+	print("\033[AGenerated " + str(i+1) + " medium files")
+os.chdir('..')
+print()
+try:
+	os.chdir('large_files')
+except:
+	os.mkdir('large_files')
+	os.chdir('large_files')
+print()
+for i in range(5):
+	f = open("large_" + str(i) + '.dat', "w+")
+	f.write(string_generator(50000000))
+	f.close()
+	print("\033[AGenerated " + str(i+1) + " large files")
+os.chdir('..')
+print()
+
+
+os.system('chmod 777 -R ' + path)
+print("Done")
--- test/test1.sh
+++ test/test1.sh
@@ -15,13 +15,13 @@ bin/client -f /tmp/socket.sk -w ${CWD}/small_files,10 -x -p -t 200
 
 bin/client -f /tmp/socket.sk -R 0 -d ${CWD}/test_output -p -t 200
 
-bin/client -f /tmp/socket.sk -W ${CWD}/medium_files/medium_0.txt -u ${CWD}/medium_files/medium_0.txt  -p -t 200 
+bin/client -f /tmp/socket.sk -W ${CWD}/medium_files/medium_0.dat -u ${CWD}/medium_files/medium_0.dat  -p -t 200 
 
-bin/client -f /tmp/socket.sk -r ${CWD}/medium_files/medium_0.txt -d ${CWD}/test_output -p -t 200 
+bin/client -f /tmp/socket.sk -r ${CWD}/medium_files/medium_0.dat -d ${CWD}/test_output -p -t 200 
 
-bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_0.txt -u ${CWD}/medium_files/medium_0.txt -p -t 2000 &
+bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_0.dat -u ${CWD}/medium_files/medium_0.dat -p -t 2000 &
 
-bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_0.txt -c ${CWD}/medium_files/medium_0.txt -p -t 200 
+bin/client -f /tmp/socket.sk -l ${CWD}/medium_files/medium_0.dat -c ${CWD}/medium_files/medium_0.dat -p -t 200 
 
 
 
