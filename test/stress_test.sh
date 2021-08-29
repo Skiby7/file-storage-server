bb57630d07b68b751ab6fd63b114a23ba3d025b6
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-08-29 13:06:32 +0200

    updated test3

--- test/stress_test.sh
+++ test/stress_test.sh
@@ -6,11 +6,8 @@ do
 	SMALL_FILE_NUM=$((RANDOM % 100))
 	MEDIUM_FILE_NUM=$((RANDOM % 10))
 	LARGE_FILE_NUM=$((RANDOM % 5))
-	bin/client -f /tmp/socket.sk -W ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -R 5 -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -W ${CWD}/test_2/initial_file_0.txt -u ${CWD}/test_2/initial_file_0.txt -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt -t 0 &> /dev/null
-	# bin/client -f /tmp/socket.sk -w ${CWD}/small_files/small_${LARGE_FILE_NUM}.txt -u ${CWD}/small_files/small_${LARGE_FILE_NUM}.txt -t 0 &> /dev/null
-	# bin/client -f /tmp/socket.sk -w ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -W ${CWD}/test_2/initial_file_0.txt -u ${CWD}/test_2/initial_file_0.txt -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt -t 0 
-	# bin/client -f /tmp/socket.sk -w ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt  -t 500 
-	# bin/client -f /tmp/socket.sk -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt &> /dev/null
+	bin/client -f /tmp/socket.sk -W ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -D ${CWD}/test_output/EVICTION_OUTPUT -u ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -R 5 -d ${CWD}/test_output/READ_OUTPUT -W ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -D ${CWD}/test_output/EVICTION_OUTPUT -u ${CWD}/medium_files/medium_${MEDIUM_FILE_NUM}.txt -W ${CWD}/test_2/initial_file_0.txt -D ${CWD}/test_output/EVICTION_OUTPUT -u ${CWD}/test_2/initial_file_0.txt -l ${CWD}/test_2/initial_file_0.txt -c ${CWD}/test_2/initial_file_0.txt -t 0 &> /dev/null
+	
 	if [[ $(($RANDOM % 2)) -eq 0 ]]
 	then
     	bin/client -f /tmp/socket.sk -l ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt -c ${CWD}/small_files/small_${SMALL_FILE_NUM}.txt &> /dev/null
@@ -20,18 +17,18 @@ do
 
 	if [[ $(($RANDOM % 10)) -eq 5 ]]
 	then
-		bin/client -f /tmp/socket.sk -w ${CWD}/medium_files -x &> /dev/null
-		bin/client -f /tmp/socket.sk -R 0 &> /dev/null
+		bin/client -f /tmp/socket.sk -w ${CWD}/medium_files -x -D ${CWD}/test_output/EVICTION_OUTPUT &> /dev/null
+		bin/client -f /tmp/socket.sk -R 0 -d ${CWD}/test_output/READ_OUTPUT &> /dev/null
 	fi
 
 	if [[ $(($RANDOM % 3)) -eq 1 ]]
 	then
-		bin/client -f /tmp/socket.sk -w ${CWD}/small_files -x &> /dev/null
+		bin/client -f /tmp/socket.sk -w ${CWD}/small_files -x -D ${CWD}/test_output/EVICTION_OUTPUT &> /dev/null
 	fi
 
 	if [[ $(($RANDOM % 100)) -eq 50 ]]
 	then
-		bin/client -f /tmp/socket.sk -W ${CWD}/large_files/large_${LARGE_FILE_NUM}.txt -u ${CWD}/large_files/large_${LARGE_FILE_NUM}.txt &> /dev/null
+		bin/client -f /tmp/socket.sk -W ${CWD}/large_files/large_${LARGE_FILE_NUM}.txt -D ${CWD}/test_output/EVICTION_OUTPUT -u ${CWD}/large_files/large_${LARGE_FILE_NUM}.txt &> /dev/null
 	fi
 
 
--- test/test3.sh
+++ test/test3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+CWD=$(realpath $(dirname $0))
+mkdir ${CWD}/test_output/READ_OUTPUT
+mkdir ${CWD}/test_output/EVICTION_OUTPUT
 
 bin/server $1 &
-
 export SERVER=$!
 sleep 2
 stress_test_pids=()
 for i in {1..15}; do
-    # bash -c './test/stress_test.sh' &
     bash -c './test/stress_test.sh' &
     stress_test_pids+=($!)
     sleep 0.1
 done
- 
 
 sleep 30
 
@@ -20,8 +20,8 @@ for i in "${stress_test_pids[@]}"; do
     wait ${i} &> /dev/null
 done
 
+
+
 kill -2 $SERVER
 wait $SERVER
-
-
 exit 0
